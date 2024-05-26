ec1bb2560dd873639524c6db3aed31c025c75fa1
wikigreen <vhrynevich20@gmail.com>
2024-05-26 12:04:51 +0300

    [frontend-setup] Initial frontend setup

--- src/main/java/org/wikigreen/springbootreactfullstacktemplate/SpringBootReactFullstackTemplateApplication.java
+++ src/main/java/org/wikigreen/springbootreactfullstacktemplate/SpringBootReactFullstackTemplateApplication.java
@@ -2,12 +2,18 @@ package org.wikigreen.springbootreactfullstacktemplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SpringBootReactFullstackTemplateApplication {
+public class SpringBootReactFullstackTemplateApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootReactFullstackTemplateApplication.class, args);
+    }
+
+    public void addViewController(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index.html");
     }
 
 }
