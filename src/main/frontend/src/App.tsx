d35bb97f34c7506398f8f0a83232ac73c0e0a353
wikigreen <vhrynevich20@gmail.com>
2024-06-01 21:42:59 +0300

    [backend-setup] Fix ping endpoint mapping

--- src/main/frontend/src/App.tsx
+++ src/main/frontend/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 	const [count, setCount] = useState(0);
 	const [pingResponse, setPingResponse] = useState("");
 	const doPing = useCallback(() => {
-		fetch("/ping")
+		fetch("api/ping")
 			.then((res) => res.text())
 			.then(setPingResponse);
 	}, []);
